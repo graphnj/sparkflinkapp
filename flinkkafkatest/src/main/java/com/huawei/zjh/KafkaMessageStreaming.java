f81c84caf2a41098e7dd88a089cb45fb93583659
zhu jinhua <jinhua1982@gmail.com>
2017-10-30 23:59:42 +0800

    max min avg

--- flinkkafkatest/src/main/java/com/huawei/zjh/KafkaMessageStreaming.java
+++ flinkkafkatest/src/main/java/com/huawei/zjh/KafkaMessageStreaming.java
@@ -2,6 +2,7 @@ package com.huawei.zjh;
 
 import org.apache.flink.core.fs.FileSystem;
 import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.WindowedStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.api.java.tuple.Tuple;
 import org.apache.flink.api.java.tuple.Tuple2;
@@ -15,12 +16,15 @@ import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer010;
 import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
 import org.apache.flink.util.Collector;
 
+import java.io.FileInputStream;
 import java.util.Properties;
 
 /**
  * Created by zhujinhua on 2017/10/29.
  */
 public class KafkaMessageStreaming {
+
+
 
     public static void main(String[] args) throws Exception {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
@@ -31,33 +35,96 @@ public class KafkaMessageStreaming {
         props.setProperty("bootstrap.servers", "localhost:9092");
         props.setProperty("group.id", "flink-group");
 
+        props.load(new FileInputStream("application.properties"));
+
+        String input_topic = props.getProperty("input_topic");
+        String output_file = props.getProperty("output_file");
+        Long   timewindows = Long.parseLong(props.getProperty("time_windows"));
+        String function = props.getProperty("function");
+
         FlinkKafkaConsumer010<String> consumer =
-                new FlinkKafkaConsumer010<>(args[0], new SimpleStringSchema(), props);
+                new FlinkKafkaConsumer010<>(input_topic, new SimpleStringSchema(), props);
         consumer.assignTimestampsAndWatermarks(new MessageWaterEmitter());
 
-        DataStream<Tuple2<String, Long>> keyedStream = env
+        /*DataStream<Tuple2<String, Long>> keyedStream = env
                 .addSource(consumer)
                 .flatMap(new MessageSplitter())
                 .keyBy(0)
-                .timeWindow(Time.seconds(10))
+                .timeWindow(Time.seconds(10));*/
 
-                .apply(new WindowFunction<Tuple2<String, Long>, Tuple2<String, Long>, Tuple, TimeWindow>() {
-                    @Override
-                    public void apply(Tuple tuple, TimeWindow window, Iterable<Tuple2<String, Long>> input, Collector<Tuple2<String, Long>> out) throws Exception {
-                        long sum = 0L;
-                        int count = 0;
-                        for (Tuple2<String, Long> record: input) {
-                            sum += record.f1;
-                            count++;
-                        }
-                        Tuple2<String, Long> result = input.iterator().next();
-                        result.f1 = sum / count;
-                        out.collect(result);
-                    }
-                });
+        WindowedStream<Tuple2<String, Long>, Tuple, TimeWindow> windowedStream = env
+                .addSource(consumer)
+                .flatMap(new MessageSplitter())
+                .keyBy(0)
+                .timeWindow(Time.seconds(timewindows));
+
+
+        WindowFunction<Tuple2<String, Long>, Tuple2<String, Long>, Tuple, TimeWindow> expectWindowFunction = null;
+
+        if (function.equals("avg")) {
+            expectWindowFunction = new windows_avg();
+        }
+        else if (function.equals("max")){
+            expectWindowFunction = new windows_max();
+        }
+        else if (function.equals("min")){
+            expectWindowFunction = new windows_min();
+        }
+
+        DataStream<Tuple2<String, Long>> keyedStream  = windowedStream.apply(expectWindowFunction);
 
         //keyedStream.writeAsText(args[1]);
-        keyedStream.writeAsText(args[1], FileSystem.WriteMode.OVERWRITE);
+        keyedStream.writeAsText(output_file, FileSystem.WriteMode.OVERWRITE);
         env.execute("Flink-Kafka demo");
+    }
+}
+
+
+class windows_avg implements WindowFunction<Tuple2<String, Long>, Tuple2<String, Long>, Tuple, TimeWindow> {
+    @Override
+    public void apply(Tuple tuple, TimeWindow window, Iterable<Tuple2<String, Long>> input, Collector<Tuple2<String, Long>> out) throws Exception {
+        long sum = 0L;
+        int count = 0;
+        for (Tuple2<String, Long> record: input) {
+            sum += record.f1;
+            count++;
+        }
+        Tuple2<String, Long> result = input.iterator().next();
+        result.f1 = sum / count;
+        out.collect(result);
+    }
+}
+
+class windows_max implements WindowFunction<Tuple2<String, Long>, Tuple2<String, Long>, Tuple, TimeWindow> {
+    @Override
+    public void apply(Tuple tuple, TimeWindow window, Iterable<Tuple2<String, Long>> input, Collector<Tuple2<String, Long>> out) throws Exception {
+        long maxval = Long.MIN_VALUE;
+
+        long sum = 0L;
+        int count = 0;
+        for (Tuple2<String, Long> record: input) {
+            if(record.f1 > maxval)
+                maxval = record.f1;
+        }
+        Tuple2<String, Long> result = input.iterator().next();
+        result.f1 = maxval;
+        out.collect(result);
+    }
+}
+
+class windows_min implements WindowFunction<Tuple2<String, Long>, Tuple2<String, Long>, Tuple, TimeWindow> {
+    @Override
+    public void apply(Tuple tuple, TimeWindow window, Iterable<Tuple2<String, Long>> input, Collector<Tuple2<String, Long>> out) throws Exception {
+        long minval = Long.MAX_VALUE;
+
+        long sum = 0L;
+        int count = 0;
+        for (Tuple2<String, Long> record: input) {
+            if(record.f1 < minval)
+                minval = record.f1;
+        }
+        Tuple2<String, Long> result = input.iterator().next();
+        result.f1 = minval;
+        out.collect(result);
     }
 }
